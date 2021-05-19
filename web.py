6646f672c4972770b03bd3058b0e5146d1be7f96
Oghenetega Akpowenre <oghenetegaakpowenre@gmail.com>
2021-05-19 10:23:52 +0100

    changed selected download link

--- web.py
+++ web.py
@@ -27,7 +27,7 @@ def download_episode(anime_name,selected_episode,episodes_dict):
     selected_episode_webpage=get_webpage(episodes_dict[selected_episode]["link"])
     downloads_link=selected_episode_webpage.findAll("li",{"class":"dowloads"})[0].a["href"]
     downloads_webpage=get_webpage(downloads_link)
-    video_link=downloads_webpage.findAll("div",{"class":"mirror_link"})[0].findAll("div",{"class":"dowload"})[0].a["href"]
+    video_link=downloads_webpage.findAll("div",{"class":"mirror_link"})[0].findAll("div",{"class":"dowload"})[2].a["href"]
     path="/home/tega/Videos/Anime/"+anime_name
     if os.path.exists(path)==False:
         os.mkdir(path)
